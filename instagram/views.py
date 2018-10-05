c9cb44bb25815de54d499032004817755c3ce6d2
James Kageni <james.kageni@gmail.com>
2018-10-05 16:24:35 +0300

    added the navbar, profile and base templates

--- instagram/views.py
+++ instagram/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'registration/login.html')
+    return render(request, 'profile.html')
--- jessi/settings.py
+++ jessi/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Email configurations remember to install python-decouple
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
