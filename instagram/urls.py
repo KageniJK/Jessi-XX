c1f0acbf4fa6292bf52c294a1109fce666bfd661
James Kageni <james.kageni@gmail.com>
2018-10-07 15:26:23 +0300

    added the search function

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'home'),
+    url('^search_results/',views.search_results,name='search_results'),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'profile.html')
+
+
+def search_results(request):
+    if 'pic' in request.GET and request.GET['pic']:
+        search_term = request.GET.get('pic')
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message': message})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {'message': message})
